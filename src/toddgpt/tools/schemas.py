e5c7cda83833e86e7943a9f410f63f19d77c88ea
pablo unzueta <punzu001@ucr.edu>
2024-08-20 16:13:01 -0700

    playing with schemas

---
+++ src/toddgpt/tools/schemas.py
@@ -0,0 +1,12 @@
+from langchain.pydantic_v1 import BaseModel, Field
+from typing import List, Optional
+
+class AtomicSystem(BaseModel):
+    energy: float = Field(..., description="Total energy of the system")
+    forces: List[List[float]] = Field(..., description="Forces on each atom as a list of [x, y, z] components")
+    symbols: List[str] = Field(..., description="Chemical symbols of the atoms")
+    positions: List[List[float]] = Field(..., description="Positions of the atoms as a list of [x, y, z] coordinates")
+
+class InterfaceInput(BaseModel):
+    system: AtomicSystem = Field(..., description="The atomic system data")
+    query: str = Field(..., description="The query string for the Interface tool")
